32113c26c9831be3326eb84b37829470c9d26d32
sromanchuk <47135579+sromanchuk@users.noreply.github.com>
2019-03-08 17:33:34 +0200

    Update sportradar_api.py

--- sportradar_api.py
+++ sportradar_api.py
@@ -1,14 +1,16 @@
 import http.client
-import xmltodict
 import json
 
-conn = http.client.HTTPSConnection("api.sportradar.us")
 
-conn.request("GET", "/tennis-t2/en/players/sr:competitor:18111/versus/sr:competitor:15126/matches.xml?api_key=8vfgwpuch3rpnauqm9cbzp7d")
+def get_json(source):
+    conn = http.client.HTTPSConnection("api.sportradar.us")
+    conn.request("GET", source)
+    res = conn.getresponse()
+    json_data = res.read().decode("utf-8")
+    json_data = json.loads(json_data)
+    return json_data
 
-res = conn.getresponse()
-data = res.read()
 
-xml_data = data.decode("utf-8")
-json_data = xmltodict.parse(xml_data)
-print(json.dumps(json_data, indent=2))
+json_data = get_json("/tennis-t2/en/players/sr:competitor:18111/versus/sr:competitor:15126/matches.json?api_key=8vfgwpuch3rpnauqm9cbzp7d")
+json_data = json.dumps(json_data, indent=2)
+print(json_data)
